9fe2c7b3c5fd7193a53668e1fae0501c3cf9e809
ahmadaoun30 <98710478+ahmadaoun30@users.noreply.github.com>
2023-06-07 17:09:22 -0400

    Update main.cpp

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -6,9 +6,49 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    double number1, number2;
+    char operation;
+
+    // input from the user
+    cout << "Enter first number: ";
+    cin >> number1;
+    
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter second number: ";
+    cin >> number2;
+
+    // Perform the calculation based on operation
+    double result;
+    switch (operation) {
+        case '+':
+            result = number1 + number2;
+            break;
+        case '-':
+            result = number1 - number2;
+            break;
+        case '*':
+            result = number1 * number2;
+            break;
+        case '/':
+            if (number2 != 0) {
+                result = number1 / number2;
+            } else {
+                cout << "Error: Division by zero is not allowed.\n";
+                return 0;
+            }
+            break;
+        default:
+            cout << "Invalid operation.\n";
+            return 0;
+    }
+
+    // Display the result
+    cout << "Result: " << result << '\n';
+
     return 0;
 }
